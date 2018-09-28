580c989ca5d99db2175b644903ed4f64293cb558
Michael Jungo <michaeljungo92@gmail.com>
2018-09-28 20:12:10 +0200

    Add tools to create the desired dataset
    InkML need to be translated to images and need to have ground truth
    information with the available tokens.

---
+++ data_tools/extract_groundtruth.py
@@ -0,0 +1,47 @@
+import argparse
+import csv
+import glob
+import os
+import xml.etree.ElementTree as ET
+
+doc_namespace = "{http://www.w3.org/2003/InkML}"
+
+
+def extract_truth(file_path):
+    root = ET.parse(file_path).getroot()
+    annotations = root.findall(doc_namespace + "annotation")
+    truths = [ann for ann in annotations if ann.get("type") == "truth"]
+    if len(truths) != 1:
+        raise Exception(
+            "{} does not contain a ground truth annotation".format(file_path))
+    return truths[0].text
+
+
+def create_tsv(path, output="groundtruth.tsv"):
+    files = glob.glob(os.path.join(path, "**/*.inkml"))
+    with open(output, "w") as fd:
+        writer = csv.writer(fd, delimiter="\t")
+        for f in files:
+            rel_path = os.path.relpath(f, path)
+            reference_name = os.path.splitext(rel_path)[0]
+            truth = extract_truth(f)
+            # Remove $ because the entire forumla is surrounded by it.
+            truth = truth.replace("$", "")
+            writer.writerow([reference_name, truth])
+
+
+if __name__ == '__main__':
+    """
+    extract_groundtruth path/to/dataset [-o OUTPUT]
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        default="groundtruth.tsv",
+        help="Output path of the TSV file")
+    parser.add_argument(
+        "directory", nargs=1, help="Directory to data with ground truth")
+    args = parser.parse_args()
+    create_tsv(args.directory[0], args.output)
---
+++ data_tools/extract_tokens.py
@@ -0,0 +1,60 @@
+import argparse
+import csv
+
+
+def parse_symbols(truth):
+    unique_symbols = set()
+    i = 0
+    while i < len(truth):
+        char = truth[i]
+        i += 1
+        if char.isspace():
+            continue
+        elif char == "\\":
+            if truth[i] == "{" or truth[i] == "}":
+                unique_symbols.add(char + truth[i])
+                i += 1
+                continue
+            escape_seq = char
+            while i < len(truth) and truth[i].isalpha():
+                escape_seq += truth[i]
+                i += 1
+            unique_symbols.add(escape_seq)
+        else:
+            unique_symbols.add(char)
+    return unique_symbols
+
+
+def create_tokens(groundtruth, output="tokens.txt"):
+    with open(groundtruth, "r") as fd:
+        unique_symbols = set()
+        reader = csv.reader(fd, delimiter="\t")
+        for _, truth in reader:
+            truth_symbols = parse_symbols(truth)
+            unique_symbols = unique_symbols.union(truth_symbols)
+
+        # This is somehow wrong, as it should be recognised as "less than N"
+        # It would be two symbols, which are both already present.
+        unique_symbols.remove("\\ltN")
+        symbols = list(unique_symbols)
+        symbols.sort()
+        with open(output, "w") as output_fd:
+            writer = csv.writer(output_fd, delimiter="\t")
+            writer.writerow(symbols)
+
+
+if __name__ == '__main__':
+    """
+    extract_tokens path/to/groundtruth.tsv [-o OUTPUT]
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        default="tokens.txt",
+        help="Output path of the tokens text file")
+    parser.add_argument(
+        "groundtruth", nargs=1, help="Ground truth TSV file")
+    args = parser.parse_args()
+    create_tokens(args.groundtruth[0], args.output)
