0f0980ee72302ee2704e8121b6506358fbe9815d
Michael Jungo <michaeljungo92@gmail.com>
2018-12-06 13:59:31 +0100

    Implement beam search for evaluation

--- evaluate.py
+++ evaluate.py
@@ -78,6 +78,55 @@ def calc_distances(actual, expected):
     ]
 
 
+def create_statistics(hypothesis):
+    sequence = hypothesis["sequence"]
+    expected = hypothesis["expected"]
+    num_tokens = hypothesis["num_tokens"]
+
+    distances_full = calc_distances(sequence["full"], expected["full"])
+    distances_removed = calc_distances(sequence["removed"], expected["removed"])
+    distances_symbols = calc_distances(sequence["symbols"], expected["symbols"])
+    distance = {
+        "full": sum(distances_full),
+        "removed": sum(distances_removed),
+        "symbols": sum(distances_symbols),
+    }
+    correct = {
+        "full": sum(
+            [
+                torch.equal(seq, exp)
+                for seq, exp in zip(sequence["full"], expected["full"])
+            ]
+        ),
+        "removed": sum(
+            [
+                torch.equal(seq, exp)
+                for seq, exp in zip(sequence["removed"], expected["removed"])
+            ]
+        ),
+        "symbols": sum(
+            [
+                torch.equal(seq, exp)
+                for seq, exp in zip(sequence["symbols"], expected["symbols"])
+            ]
+        ),
+        # This is the batch size, since each sequence is one expression.
+        "total": len(sequence["full"]),
+    }
+    hypothesis["distance"] = distance
+    hypothesis["error"] = {
+        "full": distance["full"] / num_tokens["full"],
+        "removed": distance["removed"] / num_tokens["removed"],
+        "symbols": distance["symbols"] / num_tokens["symbols"],
+    }
+    hypothesis["correct"] = correct
+    hypothesis["correct"]["percent"] = {
+        "full": correct["full"] / correct["total"],
+        "removed": correct["removed"] / correct["total"],
+        "symbols": correct["symbols"] / correct["total"],
+    }
+
+
 def to_percent(decimal, precision=2):
     after_decimal_point = 10 ** precision
     shifted = decimal * 100 * after_decimal_point
@@ -90,7 +139,7 @@ def create_markdown_tables(results):
     model_pad = max(len(key) for key in results.keys())
     model_pad = max(len(model), model_pad)
     err_token = "Token error rate"
-    err_no_special = "Token error rate (no speical tokens)"
+    err_no_special = "Token error rate (no special tokens)"
     err_symbol = "Symbol error rate"
     err_header = "| {model:>{model_pad}} | {token} | {no_special} | {symbol} |".format(
         model=model,
@@ -141,9 +190,9 @@ def create_markdown_tables(results):
             "| {symbol:>{symbol_pad}} |"
         ).format(
             model=name,
-            token=to_percent(result["correct"]["full"]),
-            no_special=to_percent(result["correct"]["removed"]),
-            symbol=to_percent(result["correct"]["symbols"]),
+            token=to_percent(result["correct"]["percent"]["full"]),
+            no_special=to_percent(result["correct"]["percent"]["removed"]),
+            symbol=to_percent(result["correct"]["percent"]["symbols"]),
             model_pad=model_pad,
             token_pad=len(correct_token),
             no_special_pad=len(correct_no_special),
@@ -156,13 +205,58 @@ def create_markdown_tables(results):
     return err_table, correct_table
 
 
-def evaluate(enc, dec, data_loader, device, checkpoint=default_checkpoint, prefix=""):
+# Picks the k unique hypotheses. They uniqueness is determined from a batch of
+# sequences. That means that several sequences in one batch may occur as duplicates
+# overall, but as soon as one doesn't already exist, the batch is kept. To have truly
+# unique sequences, it should be run with a batch size of 1.
+# TODO: Change criterion from batch wise probability to individual probabilities.
+def pick_top_k_unique(hypotheses, count):
+    unique_hypotheses = []
+    sorted_hypotheses = sorted(
+        hypotheses, key=lambda h: torch.sum(h["probability"]).item(), reverse=True
+    )
+    for h in sorted_hypotheses:
+        if len(unique_hypotheses) >= count:
+            break
+        already_exists = False
+        for h_uniq in unique_hypotheses:
+            sequence_already_exists = all(
+                [
+                    torch.equal(h_seq, h_uniq_seq)
+                    for h_seq, h_uniq_seq in zip(
+                        h["sequence"]["full"], h_uniq["sequence"]["full"]
+                    )
+                ]
+            )
+            if sequence_already_exists:
+                already_exists = True
+                break
+        if not already_exists:
+            unique_hypotheses.append(h)
+    return unique_hypotheses
+
+
+def evaluate(
+    enc,
+    dec,
+    data_loader,
+    device,
+    checkpoint=default_checkpoint,
+    beam_width=10,
+    prefix="",
+):
     special_tokens = [data_loader.dataset.token_to_id[tok] for tok in SPECIAL_TOKENS]
     non_symbols_encoded = [data_loader.dataset.token_to_id[tok] for tok in non_symbols]
-    correct_tokens = 0
-    distance = {"full": 0, "removed": 0, "symbols": 0}
-    num_tokens = {"full": 0, "removed": 0, "symbols": 0}
-    correct = {"full": 0, "removed": 0, "symbols": 0, "total": 0}
+    best = {
+        "num_tokens": {"full": 0, "removed": 0, "symbols": 0},
+        "distance": {"full": 0, "removed": 0, "symbols": 0},
+        "correct": {"full": 0, "removed": 0, "symbols": 0, "total": 0},
+    }
+    mean = {
+        "num_tokens": {"full": 0, "removed": 0, "symbols": 0},
+        "distance": {"full": 0, "removed": 0, "symbols": 0},
+        "correct": {"full": 0, "removed": 0, "symbols": 0, "total": 0},
+    }
 
     for d in data_loader:
         input = d["image"].to(device)
@@ -184,70 +278,141 @@ def evaluate(enc, dec, data_loader, device, checkpoint=default_checkpoint, prefi
             dtype=torch.long,
             device=device,
         )
-        decoded_values = []
+        hypotheses = [
+            {
+                "sequence": {"full": sequence},
+                "hidden": hidden,
+                "attn": {
+                    "low": dec.coverage_attn_low.alpha,
+                    "high": dec.coverage_attn_high.alpha,
+                },
+                # This will be a tensor of probabilities (one for each batch), but at
+                # the beginning it can be 1.0 because it will be broadcast for the
+                # multiplication and it means the first tensor of probabilities will be
+                # kept as is.
+                "probability": 1.0,
+            }
+        ]
         for i in range(batch_max_len - 1):
-            previous = sequence[:, -1].view(-1, 1)
-            out, hidden = dec(previous, hidden, enc_low_res, enc_high_res)
-            _, top1_id = torch.topk(out, 1)
-            sequence = torch.cat((sequence, top1_id), dim=1)
-            decoded_values.append(out)
-        decoded_values = torch.stack(decoded_values, dim=2).to(device)
+            step_hypotheses = []
+            for hypothesis in hypotheses:
+                curr_sequence = hypothesis["sequence"]["full"]
+                previous = curr_sequence[:, -1].view(-1, 1)
+                curr_hidden = hypothesis["hidden"]
+                # Set the attention to the corresponding values, otherwise it would use
+                # the attention from another hypothesis.
+                dec.coverage_attn_low.alpha = hypothesis["attn"]["low"]
+                dec.coverage_attn_high.alpha = hypothesis["attn"]["high"]
+                out, next_hidden = dec(previous, curr_hidden, enc_low_res, enc_high_res)
+                probabilities = torch.softmax(out, dim=1)
+                topk_probs, topk_ids = torch.topk(probabilities, beam_width)
+                # topks are transposed, because the columns are needed, not the rows.
+                # One column is the top values for the batches, and there are k rows.
+                for top_prob, top_id in zip(topk_probs.t(), topk_ids.t()):
+                    next_sequence = torch.cat(
+                        (curr_sequence, top_id.view(-1, 1)), dim=1
+                    )
+                    probability = hypothesis["probability"] * top_prob
+                    next_hypothesis = {
+                        "sequence": {"full": next_sequence},
+                        "hidden": next_hidden,
+                        "attn": {
+                            "low": dec.coverage_attn_low.alpha,
+                            "high": dec.coverage_attn_high.alpha,
+                        },
+                        "probability": probability,
+                    }
+                    step_hypotheses.append(next_hypothesis)
+            # Only the beam_width number of hypotheses with the highest probabilities
+            # are kept for the next iteration.
+            hypotheses = pick_top_k_unique(step_hypotheses, beam_width)
 
-        sequence_removed = [
-            remove_special_tokens(seq, special_tokens) for seq in sequence
-        ]
-        sequence_symbols = [
-            remove_special_tokens(seq, non_symbols_encoded) for seq in sequence_removed
-        ]
         expected_removed = [
             remove_special_tokens(exp, special_tokens) for exp in expected
         ]
         expected_symbols = [
             remove_special_tokens(exp, non_symbols_encoded) for exp in expected_removed
         ]
-        correct_tokens += torch.sum(sequence == expected, dim=(0, 1)).item()
-        distances_full = calc_distances(sequence, expected)
-        distances_removed = calc_distances(sequence_removed, expected_removed)
-        distances_symbols = calc_distances(sequence_symbols, expected_symbols)
-        distance["full"] += sum(distances_full)
-        distance["removed"] += sum(distances_removed)
-        distance["symbols"] += sum(distances_symbols)
-        correct["full"] += sum(
-            [torch.equal(seq, exp) for seq, exp in zip(sequence, expected)]
-        )
-        correct["removed"] += sum(
-            [
-                torch.equal(seq, exp)
-                for seq, exp in zip(sequence_removed, expected_removed)
-            ]
-        )
-        correct["symbols"] += sum(
-            [
-                torch.equal(seq, exp)
-                for seq, exp in zip(sequence_symbols, expected_symbols)
-            ]
-        )
-        correct["total"] += len(expected_symbols)
-
         # Can't use .numel() for the removed versions, because they can't
         # be converted to a tensor (stacked), as they may not have the same length.
         # Instead it's a list of tensors.
-        num_tokens["full"] += expected.numel()
-        num_tokens["removed"] += sum([exp.numel() for exp in expected_removed])
-        num_tokens["symbols"] += sum([exp.numel() for exp in expected_symbols])
+        num_tokens = {
+            "full": expected.numel(),
+            "removed": sum([exp.numel() for exp in expected_removed]),
+            "symbols": sum([exp.numel() for exp in expected_symbols]),
+        }
+        for hypothesis in hypotheses:
+            sequence = hypothesis["sequence"]
+            sequence["removed"] = [
+                remove_special_tokens(seq, special_tokens) for seq in sequence["full"]
+            ]
+            sequence["symbols"] = [
+                remove_special_tokens(seq, non_symbols_encoded)
+                for seq in sequence["removed"]
+            ]
+            hypothesis["expected"] = {
+                "full": expected,
+                "removed": expected_removed,
+                "symbols": expected_symbols,
+            }
+            hypothesis["num_tokens"] = num_tokens
+            create_statistics(hypothesis)
 
-    return {
-        "error": {
-            "full": distance["full"] / num_tokens["full"],
-            "removed": distance["removed"] / num_tokens["removed"],
-            "symbols": distance["symbols"] / num_tokens["symbols"],
-        },
-        "correct": {
-            "full": correct["full"] / correct["total"],
-            "removed": correct["removed"] / correct["total"],
-            "symbols": correct["symbols"] / correct["total"],
-        },
+        for category in ["full", "removed", "symbols"]:
+            category_distance = torch.tensor(
+                [hypothesis["distance"][category] for hypothesis in hypotheses],
+                dtype=torch.float,
+            )
+            category_correct = torch.tensor(
+                [hypothesis["correct"][category] for hypothesis in hypotheses],
+                dtype=torch.float,
+            )
+            category_correct_total = torch.tensor(
+                [hypothesis["correct"]["total"] for hypothesis in hypotheses],
+                dtype=torch.float,
+            )
+            category_num_tokens = torch.tensor(
+                [hypothesis["num_tokens"][category] for hypothesis in hypotheses],
+                dtype=torch.float,
+            )
+            best_distance = torch.argmin(category_distance)
+            best_correct = torch.argmax(category_correct)
+            best["distance"][category] += hypotheses[best_distance]["distance"][
+                category
+            ]
+            best["num_tokens"][category] += hypotheses[best_distance]["num_tokens"][
+                category
+            ]
+            best["correct"][category] += hypotheses[best_correct]["correct"][category]
+            best["correct"]["total"] += hypotheses[best_correct]["correct"]["total"]
+            mean["distance"][category] += torch.mean(category_distance).item()
+            mean["num_tokens"][category] += torch.mean(category_num_tokens).item()
+            mean["correct"][category] += torch.mean(category_correct).item()
+            # This should be constant, as every hypothesis should contain the same
+            # number of expressions and therefore the mean should also be the same.
+            mean["correct"]["total"] += torch.mean(category_correct_total).item()
+
+    best["error"] = {
+        "full": best["distance"]["full"] / best["num_tokens"]["full"],
+        "removed": best["distance"]["removed"] / best["num_tokens"]["removed"],
+        "symbols": best["distance"]["symbols"] / best["num_tokens"]["symbols"],
     }
+    best["correct"]["percent"] = {
+        "full": best["correct"]["full"] / best["correct"]["total"],
+        "removed": best["correct"]["removed"] / best["correct"]["total"],
+        "symbols": best["correct"]["symbols"] / best["correct"]["total"],
+    }
+    mean["error"] = {
+        "full": mean["distance"]["full"] / mean["num_tokens"]["full"],
+        "removed": mean["distance"]["removed"] / mean["num_tokens"]["removed"],
+        "symbols": mean["distance"]["symbols"] / mean["num_tokens"]["symbols"],
+    }
+    mean["correct"]["percent"] = {
+        "full": mean["correct"]["full"] / mean["correct"]["total"],
+        "removed": mean["correct"]["removed"] / mean["correct"]["total"],
+        "symbols": mean["correct"]["symbols"] / mean["correct"]["total"],
+    }
+    return {"best": best, "mean": mean}
 
 
 def parse_args():
@@ -310,7 +475,7 @@ def main():
     device = torch.device(hardware)
 
     for dataset_name in options.dataset:
-        results = {}
+        results = {"best": {}, "mean": {}}
         for checkpoint_path in options.checkpoint:
             checkpoint_name, _ = os.path.splitext(os.path.basename(checkpoint_path))
             checkpoint = (
@@ -355,12 +520,22 @@ def main():
                 checkpoint=checkpoint,
                 prefix=options.prefix,
             )
-            results[checkpoint_name] = result
+            results["best"][checkpoint_name] = result["best"]
+            results["mean"][checkpoint_name] = result["mean"]
 
-        err_table, correct_table = create_markdown_tables(results)
+        best_err_table, best_correct_table = create_markdown_tables(results["best"])
+        mean_err_table, mean_correct_table = create_markdown_tables(results["mean"])
         print(
-            "\n# Dataset {name}\n\n{err_table}\n\n{correct_table}".format(
-                name=dataset_name, err_table=err_table, correct_table=correct_table
+            (
+                "\n# Dataset {name}\n\n"
+                "## Best\n\n{best_err_table}\n\n{best_correct_table}\n\n"
+                "## Mean\n\n{mean_err_table}\n\n{mean_correct_table}"
+            ).format(
+                name=dataset_name,
+                best_err_table=best_err_table,
+                best_correct_table=best_correct_table,
+                mean_err_table=mean_err_table,
+                mean_correct_table=mean_correct_table,
             )
         )
 
